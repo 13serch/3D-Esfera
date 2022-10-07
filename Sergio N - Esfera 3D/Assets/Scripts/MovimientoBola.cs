d37f4019f3156e943fbe6a51ec57aa6b1804b358
David Bolívar <sergio13nb24@gmail.com>
2022-10-07 13:40:47 +0100

    Esfera 3D

---
+++ Sergio N - Esfera 3D/Assets/Scripts/MovimientoBola.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoBola : MonoBehaviour
+{
+
+    public float movimientoEjeX;
+    public float movimientoEjeY;
+    public float movimientoEjeZ;
+
+
+    public float velocidadMovimiento = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movimientoEjeX = Input.GetAxis("Horizontal") * Time.deltaTime * velocidadMovimiento;
+        movimientoEjeZ = Input.GetAxis("Vertical") * Time.deltaTime * velocidadMovimiento;
+        transform.Translate(movimientoEjeX, movimientoEjeY, movimientoEjeZ);
+
+    }
+}

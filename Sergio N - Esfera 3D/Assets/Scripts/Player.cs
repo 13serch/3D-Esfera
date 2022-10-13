36b88a9668ed6d1ff1be9fc9fdb0762c54b362a0
David Bolívar <sergio13nb24@gmail.com>
2022-10-13 12:25:14 +0100

    Esfera 3D

--- Sergio N - Esfera 3D/Assets/Scripts/Player.cs
+++ Sergio N - Esfera 3D/Assets/Scripts/Player.cs
@@ -2,36 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Moneda : MonoBehaviour
+public class Player : MonoBehaviour
 {
+
+    public int monedas = 10;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (monedas <= 0)
+        {
+            Over.show();
 
+        }
     }
-
-
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        
-        
-
-    }
-
-
-
-
-
-
-
-
 }
-
-
